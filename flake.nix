1d82f63842225dd3941f772c0f67fb92598b768e
Justin Hoang <72476123+suasuasuasuasua@users.noreply.github.com>
2026-01-05 18:38:30 -0700

    build(nixpkgs): pin version to 25.11 (#30)

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     git-hooks.url = "github:cachix/git-hooks.nix";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
