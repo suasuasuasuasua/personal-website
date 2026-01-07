b07d0a3312f0a9517ec4e48fe75ea881aaf47450
Justin Hoang <justinhoang@sua.sh>
2026-01-06 20:07:50 -0700

    fix(treefmt): add frontmatter plugin

--- treefmt.nix
+++ treefmt.nix
@@ -9,7 +9,10 @@
   programs = {
     jsonfmt.enable = true;
     just.enable = true;
-    mdformat.enable = true;
+    mdformat = {
+      enable = true;
+      plugins = ps: [ ps.mdformat-frontmatter ];
+    };
     nixfmt.enable = pkgs.lib.meta.availableOn pkgs.stdenv.buildPlatform pkgs.nixfmt-rfc-style.compiler;
     nixfmt.package = pkgs.nixfmt-rfc-style;
     taplo.enable = true;
