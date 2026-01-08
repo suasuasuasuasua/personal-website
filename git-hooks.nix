7173b4919cedf13e7e7eb2607fe2161319903a98
Justin Hoang <72476123+suasuasuasuasua@users.noreply.github.com>
2026-01-07 20:33:46 -0700

    46 add spell checker linting step (#50)
    * ci(hooks): add github action linters
    
    * fix(treefmt): use simple breaks
    
    3 dashes instead of 70 underscores
    https://github.com/csala/mdformat-simple-breaks
    https://mdformat.readthedocs.io/en/stable/users/style.html#thematic-breaks
    
    * chore(vale): add program
    
    * chore(prettier): add hook
    
    * feat(vale): add styles
    
    redhat, proselint, write-good, hugo
    
    * feat: lint the existing blog posts
    
    * chore: enable prettier on treefmt

--- git-hooks.nix
+++ git-hooks.nix
@@ -1,11 +1,17 @@
 {
   hooks = {
+    action-validator.enable = true;
+    actionlint.enable = true;
     check-merge-conflicts.enable = true;
     commitizen.enable = true;
     deadnix.enable = true;
     end-of-file-fixer.enable = true;
     markdownlint.enable = true;
     nixfmt-rfc-style.enable = true;
+    # TODO: figure out prettier cause it's having weird interactions with the
+    # other formatters
+    # prettier.enable = true;
     trim-trailing-whitespace.enable = true;
+    vale.enable = true;
   };
 }
